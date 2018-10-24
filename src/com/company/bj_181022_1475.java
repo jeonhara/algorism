0106af5555e5f425c1a725b6c855ea35c91353e9
jeonhara <un9565650@gmail.com>
2018-10-24 10:26:53 +0900

    백준1475번 커밋하라

--- src/com/company/bj_181022_1475.java
+++ src/com/company/bj_181022_1475.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class bj_181022_1475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String num = st.nextToken();
+        System.out.println(num.charAt(0));
+        System.out.println(num.length());
+
+
+
+
+    }
 }
