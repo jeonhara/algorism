c2e3d6cd57a161ce2aeb0acf03dc2ff0e55a94dc
jeonhara <un9565650@gmail.com>
2018-12-06 16:19:03 +0900

    백준1475번 커밋하라

--- src/com/company/bj_181022_1475.java
+++ src/com/company/bj_181022_1475.java
@@ -3,7 +3,6 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class bj_181022_1475 {
@@ -11,11 +10,42 @@ public class bj_181022_1475 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         String num = st.nextToken();
-        System.out.println(num.charAt(0));
-        System.out.println(num.length());
+        int[] flag = new int[10];
+        for(int i=0; i<10; i++){
+            flag[i] = 1;
+        }
 
-
-
-
+        int sum = 1;
+        for(int i=0; i<num.length(); i++){
+            if(num.charAt(i)-'0' == 6 || num.charAt(i)-'0' == 9){
+                if(flag[num.charAt(i)-'0'] == 1){
+                    flag[num.charAt(i)-'0'] = 0;
+                }else{
+                    if(num.charAt(i)-'0' == 6 && flag[9] == 1){
+                        flag[9] = 0;
+                    }else if(num.charAt(i)-'0' == 9 && flag[6] == 1){
+                        flag[6] = 0;
+                    }else{
+                        sum++;
+                        if(num.charAt(i)-'0' == 6){
+                            flag[9] = 1;
+                        }else{
+                            flag[6] = 1;
+                        }
+                    }
+                }
+            }else{
+                if(flag[num.charAt(i)-'0'] == 1){
+                    flag[num.charAt(i)-'0'] = 0;
+                }else{
+                    sum++;
+                    for(int j=0; j<10; j++){
+                        flag[j] = 1;
+                    }
+                    flag[num.charAt(i)-'0'] = 0;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
