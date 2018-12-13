475023be27bebb790cb9ba2a993183a4ffcf5cce
jeonhara <un9565650@gmail.com>
2018-12-13 14:54:56 +0900

    네수 풀었음 커밋하라

---
+++ src/baekjoon/bj_181213_10824.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_181213_10824 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String c = st.nextToken();
+        String d = st.nextToken();
+        System.out.println(Long.valueOf(a+b)+Long.valueOf(c+d));
+    }
+}
