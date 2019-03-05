7ab7438efc2df650daead35889ac49f3d944e71b
jeonhara <67>
2019-03-05 16:56:23 +0900

    커밋하라

---
+++ src/baekjoon/bj_181220_2941.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class bj_181220_2941 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add("c=");
+        arr.add("c-");
+        arr.add("dz=");
+        arr.add("lj");
+        arr.add("nj");
+        arr.add("s=");
+        arr.add("z=");
+
+        String str = br.readLine();
+        int cnt = 0;
+        int sum = 0;
+        while(true){
+            str.charAt(cnt);
+        }
+    }
+}
