d5a8145cdfb3e899f4f042eb831b1177a51cf6cd
전하라 <jhara0714@gmail.com>
2020-12-15 15:26:22 +0900

    커밋하라

---
+++ src/baekjoon/pg_hash_전화번호목록.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class pg_hash_전화번호목록 {
+    public static void main(String[] args) throws IOException {
+        String[] array = new String[3];
+        array[0] = "1195524421";
+        array[1] = "119";
+        array[2] = "97674223";
+
+        solution(array);
+    }
+
+    public static boolean solution(String[] phone_book) {
+
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i ++) {
+            for (int j = i + 1; j < phone_book.length; j ++) {
+                if (phone_book[j].startsWith(phone_book[i])) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
