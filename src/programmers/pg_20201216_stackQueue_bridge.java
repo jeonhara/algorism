d6c5b0732a6a3fb57199cd46ad025be36e042f74
전하라 <jhara0714@gmail.com>
2020-12-18 09:00:38 +0900

    20201216 프로그래머스 스택/큐 알고리즘 - 다리를지나는 트럭 하는중

--- src/programmers/pg_20201216_stackQueue_bridge.java
+++ src/programmers/pg_20201216_stackQueue_bridge.java
@@ -5,32 +5,67 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class pg_20201216_stackQueue_bridge {
-    public static void main(String[] args) throws IOException {
-        int bridge_length = 2;
-        int weight = 10;
-        int[] prices = {7,4,5,6};
-        solution(bridge_length, weight, prices);
+  public static void main(String[] args) throws IOException {
+    int bridge_length = 100;
+    int weight = 100;
+    int[] prices = {10,10,10,10,10,10,10,10,10,10};
+    System.out.println(solution(bridge_length, weight, prices));
+  }
+
+  public static int solution(int bridge_length, int weight, int[] truck_weights) {
+    int answer = 0;
+
+    Queue<Integer> stanbyQue = new LinkedList<>();
+    for (int truck_weight : truck_weights) {
+        stanbyQue.add(truck_weight);
     }
 
-    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+    Queue<Integer> crossQue = new LinkedList<>();
+    Queue<Integer> crossLengthQue = new LinkedList<>();
+    int truckWeight = 0;
+    int sumWeight = 0;
+    while (true) {
+      answer ++;
 
-        int weightSum = 0;
-        int second = 0;
-        Queue<Integer> truckQue = new LinkedList<>();
+      if (!crossQue.isEmpty()) {
+        int crossLength = crossLengthQue.peek() + 1;
+        if (crossLength > bridge_length) {
+            sumWeight -= crossQue.peek();
+        }
+      }
 
-        for (int truck_weight : truck_weights) {
-            for (int i = 0; i < bridge_length; i ++) {
-                second ++;
-                weightSum += truck_weight;
+      if (!stanbyQue.isEmpty()) {
+        truckWeight = stanbyQue.peek();
+        sumWeight += truckWeight;
+        if (sumWeight <= weight) {
+            stanbyQue.remove();
+            crossQue.add(truckWeight);
+            crossLengthQue.add(0);
 
-                if (weight >= weightSum) {
-                    truckQue.add(truck_weight);
-                }
-            }
+        } else {
+            sumWeight -= truckWeight;
+        }
+      }
+
+      if (!crossQue.isEmpty()) {
+        int size = crossLengthQue.size();
+        for (int i = 0; i < size; i ++) {
+          int crossLength = crossLengthQue.peek() + 1;
+          if (crossLength > bridge_length) {
+              crossQue.remove();
+              crossLengthQue.remove();
+              continue;
+          }
+
+          crossLengthQue.add(crossLengthQue.poll() + 1);
         }
 
-
-        int answer = 0;
-        return answer;
+      } else {
+        answer --;
+        break;
+      }
     }
+
+    return answer;
+  }
 }
